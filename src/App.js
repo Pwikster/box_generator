0b95e10956fd12800e06d0afb7065adc9003d417
Pwikster <pwkjr1@gmail.com>
2023-11-16 19:23:31 -0600

    Removing old comments

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import BoxForm from './components/BoxForm';
 import DisplayBox from './components/DisplayBox';
 
-//TODO add state and function for retreiving box size and color
-
 function App() {
   const [currentBoxes, setCurrentBoxes] = useState([])
 
